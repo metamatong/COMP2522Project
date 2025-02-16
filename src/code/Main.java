37262e54bbc6ffc1fa107882ca4b7080ec27f32a
Kyle Cheon <kcheon@my.bcit.ca>
2025-02-15 16:54:34 -0800

    feat: create Main class with a skeleton

---
+++ src/code/Main.java
@@ -0,0 +1,20 @@
+/**
+ * A class that drives users to play three games presented by this project.
+ *
+ * @author Kyle Cheon
+ * @version 1.0
+ */
+class Main
+{
+    private static void main(final String[] args)
+    {
+//        Offers a menu (in the terminal; i.e. no GUI) in an infinite loop until the user presses
+//        Q (or q):
+//        Press W to play the Word game.
+//            Press N to play the Number game.
+//        Press M to play the <your game's name> game.
+//        Press Q to quit.
+//        If the user enters wrong data (not w, W, n, N, M, m, q, or Q), give an error message
+//        and re-prompt.
+    }
+}
